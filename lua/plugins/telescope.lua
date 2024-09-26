6fef3490307bcf6224201f98ff53a8c51f0a96c9
Stefan Arentz <stefan@arentz.ca>
2024-09-26 21:29:03 +0000

    Adding lua/plugins/telescope.lua

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        file_ignore_patterns = {
+          ".venv",
+          "__pycache__",
+          ".git",
+        },
+      },
+    },
+  },
+}
